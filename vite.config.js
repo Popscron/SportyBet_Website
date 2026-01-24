b139e4a5fc361146f479d9e254448b90d030b8fe
Popscron <hansonfiifi77@gmail.com>
2026-01-24 09:25:39 +0100

    Update SportyBet website: modified components and configuration files

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,18 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Vendor chunks - split large libraries
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui-vendor': ['react-icons', 'react-toastify'],
+          'utils-vendor': ['axios'],
+        },
+      },
+    },
+    // Increase chunk size warning limit to 600KB (or remove warning if acceptable)
+    chunkSizeWarningLimit: 600,
+  },
 });
